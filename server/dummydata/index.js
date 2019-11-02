80248e144eb09ad4343085715bb4d3acca33a92b
Josemaria <josemarianriagu25@gmail.com>
2019-11-02 20:09:38 +0800

    define dummy data

---
+++ server/dummydata/index.js
@@ -0,0 +1,17 @@
+module.exports = [
+   {
+      name: 'Name of the Wind',
+      genre: 'Fantasy',
+      id: '1'
+   },
+   {
+      name: 'The Final Empire',
+      genre: 'Fantasy',
+      id: '2'
+   },
+   {
+      name: 'The Long Earth',
+      genre: 'Sci-Fi',
+      id: '3'
+   }
+]
