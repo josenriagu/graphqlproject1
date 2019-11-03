f78a30eec2fa34abca49f2715e2ec777c1c4d44f
Josemaria <josemarianriagu25@gmail.com>
2019-11-03 14:07:47 +0800

    refactor and add authors data

--- server/dummydata/index.js
+++ server/dummydata/index.js
@@ -1,17 +1,38 @@
-module.exports = [
-   {
-      name: 'Name of the Wind',
-      genre: 'Fantasy',
-      id: '1'
-   },
-   {
-      name: 'The Final Empire',
-      genre: 'Fantasy',
-      id: '2'
-   },
-   {
-      name: 'The Long Earth',
-      genre: 'Sci-Fi',
-      id: '3'
-   }
-]
+
+
+module.exports = {
+   books: [
+      {
+         name: 'Name of the Wind',
+         genre: 'Fantasy',
+         id: '1'
+      },
+      {
+         name: 'The Final Empire',
+         genre: 'Fantasy',
+         id: '2'
+      },
+      {
+         name: 'The Long Earth',
+         genre: 'Sci-Fi',
+         id: '3'
+      }
+   ],
+   authors: [
+      {
+         name: 'Patrick Rothfuss',
+         age: 44,
+         id: '1'
+      },
+      {
+         name: 'Brandon Sanderson',
+         age: 42,
+         id: '2'
+      },
+      {
+         name: 'Terry Pratchett',
+         age: 66,
+         id: '3'
+      }
+   ]
+}
