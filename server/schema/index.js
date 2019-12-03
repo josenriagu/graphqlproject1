0caf13d8a3bedad424a34e2dcb992c4f94537f3b
Josemaria Nriagu <josemarianriagu25@gmail.com>
2019-12-03 21:38:29 +0800

    define Mutation

--- server/schema/index.js
+++ server/schema/index.js
@@ -1,6 +1,8 @@
 const graphql = require('graphql');
 const _ = require("lodash");
-const data = require('../dummydata');
+const Book = require('../models/book');
+const Author = require('../models/author');
+// const data = require('../dummydata');
 
 // step 1: grab the required properties from the graphql package
 const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLInt, GraphQLSchema, GraphQLList } = graphql;
@@ -16,7 +18,7 @@ const BookType = new GraphQLObjectType({
       author: {
          type: AuthorType,
          resolve(parent, args) {
-            return _.find(data.authors, { id: parent.authorId })
+            // return _.find(data.authors, { id: parent.authorId })
          }
       }
    })
@@ -31,7 +33,7 @@ const AuthorType = new GraphQLObjectType({
       books: {
          type: new GraphQLList(BookType),
          resolve(parent, args) {
-            return _.filter(data.books, { authorId: parent.id })
+            // return _.filter(data.books, { authorId: parent.id })
          }
       }
    })
@@ -43,36 +45,61 @@ const RootQuery = new GraphQLObjectType({
    fields: {
       book: {
          type: BookType,
-         args: { id: { type: GraphQLID } },
+         args: {
+            id: { type: GraphQLID }
+         },
          resolve(parent, args) {
-            return _.find(data.books, { id: args.id })
+            // return _.find(data.books, { id: args.id })
          }
       },
       author: {
          type: AuthorType,
-         args: { id: { type: GraphQLID } },
+         args: {
+            id: { type: GraphQLID }
+         },
          resolve(parent, args) {
-            return _.find(data.authors, { id: args.id })
+            // return _.find(data.authors, { id: args.id })
          }
       },
       books: {
          type: new GraphQLList(BookType),
          resolve(parent, args) {
-            return data.books
+            // return data.books
          }
       },
       authors: {
          type: new GraphQLList(AuthorType),
          resolve(parent, args) {
-            return data.authors
+            // return data.authors
          }
       }
 
    }
 })
 
+const Mutation = new GraphQLObjectType({
+   name: 'Mutation',
+   fields: {
+      addAuthor: {
+         type: AuthorType,
+         args: {
+            name: { type: GraphQLString },
+            age: { type: GraphQLInt }
+         },
+         resolve(parent, args) {
+            let author = new Author({
+               name: args.name,
+               age: args.age
+            });
+            return author.save();
+         }
+      }
+   }
+})
+
 module.exports = new GraphQLSchema({
-   query: RootQuery
+   query: RootQuery,
+   mutation: Mutation
 })
 
 // GraphQLString lets you pass only strings as parameters unlike GraphQLID which is a little more flexible and works even if the parameter is integer or string
